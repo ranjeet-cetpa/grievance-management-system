756a385beae8a6cd434b8640b0bcea4cccaa6c70
ranjeet-cetpa <ranjeet@cetpainfotech.com>
2025-04-13 17:00:40 +0530

    ui created

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,6 @@
 import { log } from 'console';
 import { logout } from './features/auth/authSlice';
 
-const DEV_CONFIG = {
-  apiUrl: 'https://tms-services.cetpainfotech.com/api',
-  orgHierarchy: 'https://uat.dfccilorganization.services.cetpainfotech.com/api',
-  loginUrl: 'https://uat.tourservices.cetpainfotech.com/api',
-  logoutUrl: 'http://localhost:3001/',
-  powerOffUrl: 'http://uat.dfccil.com/DfcHome',
-};
-const CETPA_UAT = {
-  baseUrl: 'https://gms.cetpainfotech.com',
-  apiUrl: 'https://uat.grivance.services.dfccil.cetpainfotech.com/api',
-  orgHierarchy: 'https://uat.dfccilorganization.services.cetpainfotech.com/api',
-  loginUrl: 'https://uat.tourservices.cetpainfotech.com/api',
-  logoutUrl: 'https://gms.cetpainfotech.com',
-  powerOffUrl: 'http://uat.dfccil.com/DfcHome',
-};
-
 const DFCCIL_UAT = {
   apiUrl: 'https://uatgrievancesservices.dfccil.com/api',
   orgHierarchy: 'https://uatorganization.dfccil.com/api',
@@ -31,4 +15,4 @@ const PROD_DFCCIL = {
   powerOffUrl: 'http://uat.dfccil.com/DfcHome',
 };
 
-export const environment = CETPA_UAT;
+export const environment = import.meta.env.VITE_ENV === 'production' ? PROD_DFCCIL : DFCCIL_UAT;
